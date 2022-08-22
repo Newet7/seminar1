cbc3b6239742d0b19c016df40e4b21616adc7049
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-22 21:47:08 +0300

    task 0.1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//double a;
+//string input;
+//double result;
+//input = Console.ReadLine();
+//a = Double.Parse(input);
+//result = a * a;
+//Console.Write("result square a is", result);
+//Console.Write( a * a //);
+
+int num1 = 36;
+int num2 = 6;
+int num2_squared = num2*num2;
+if (num1 == num2_squared)
+{
+    Console.Write("YES");
+}
+else
+{
+    Console.Write("NO");
+}
