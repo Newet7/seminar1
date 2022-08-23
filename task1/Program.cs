119063065241258d198f2751f5ae989a236392c9
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-23 20:59:18 +0300

    task1final

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,9 @@
-﻿int num1 = 5;
-int num2 = 7;
+﻿//int num1 = 5;
+//int num2 = 7 ;
+//int num1 = 2;
+//int num2 = 10 ;
+int num1 = -9;
+int num2 = -3 ;
 //int num2_squared = num2*num2;
 if (num1 < num2)
 {
