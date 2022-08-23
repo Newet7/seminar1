2a8f932502f34f961b63783269cb34078690a3c6
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-23 21:56:43 +0300

    task2final

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿//int [] num1 = {2, 3, 7};
+//int maxValue = num1.Max<int>();
+//int[] num1 = {2, 3, 7};
+//size=3;
+//i=0;
+//int firstmaxnum = num1[i];
+//int secondmaxnum = num1[i];
+//while (i<size) do
+//    if (num1[i]>secondmaxnum) Then;
+
+//int a = 2;
+//int b = 3;
+//int c = 7;
+
+//int a = 44;
+//int b = 5;
+//int c = 78;
+
+int a = 22;
+int b = 3;
+int c = 9;
+
+int max=a;
+if (b>max)max=b;
+if (c>max)max=c;
+Console.Write(max);
+
