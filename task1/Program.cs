8298c72d347e686ebad025388e1a34ab5b8ac77a
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-23 21:40:22 +0300

    task1 final2

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,14 +2,21 @@
 //int num2 = 7 ;
 //int num1 = 2;
 //int num2 = 10 ;
-int num1 = -9;
-int num2 = -3 ;
-//int num2_squared = num2*num2;
-if (num1 < num2)
-{
-    Console.Write(num2);
-}
-else
-{
-    Console.Write(num1);
-}
+//int num1 = -9;
+//int num2 = -3 ;
+////int num2_squared = num2*num2;
+//if (num1 < num2)
+//{
+//    Console.Write(num2);
+//}
+//else
+//{
+//    Console.Write(num1);
+//}
+
+  var n1 = Decimal.Parse(Console.ReadLine());
+            var n2 = Decimal.Parse(Console.ReadLine());
+            if (n1 > n2)
+                Console.WriteLine(n1);
+            else 
+                Console.WriteLine(n2);
