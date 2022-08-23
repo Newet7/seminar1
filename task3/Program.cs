b4baa5151fc8bf03eea4f8fb79a120eea07f31f6
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-23 22:13:20 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿double a;
+string input;
+double result;
+input = Console.ReadLine();
+a = Double.Parse(input);
+result = a % 2;
+Console.Write("число чётное?", result);
+Console.Write( a % 2 == 0 );
