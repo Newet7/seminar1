076c3aeb533db4eef2f6c3ef497b471716eecda7
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-27 21:14:13 +0300

    task2_finalfinal

--- task2/Program.cs
+++ task2/Program.cs
@@ -16,12 +16,48 @@
 //int b = 5;
 //int c = 78;
 
-int a = 22;
-int b = 3;
-int c = 9;
+//int a = 22;
+//int b = 3;
+//int c = 9;
 
-int max=a;
-if (b>max)max=b;
-if (c>max)max=c;
-Console.Write(max);
+//int max=a;
+//if (b>max)max=b;
+//if (c>max)max=c;
+//Console.Write(max);
 
+int a;
+int b;
+int c;
+int max;
+string input1;
+string input2;
+string input3;
+
+
+input1 = Console.ReadLine();
+input2 = Console.ReadLine();
+input3 = Console.ReadLine();
+
+a = Int32.Parse(input1);
+b = Int32.Parse(input2);
+c = Int32.Parse(input3);
+
+//max = a < b;
+
+if (a < b)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+
+if (max < c)
+{
+    Console.Write("max =" + c);
+}
+else
+{
+    Console.Write("max =" + max);
+}
