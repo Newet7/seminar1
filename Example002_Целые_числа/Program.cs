1848ef6f2a73d8df9cb54dd92ba70764abdd76e1
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-28 19:08:34 +0300

    find max in целое number

---
+++ Example002_Целые_числа/Program.cs
@@ -0,0 +1,17 @@
+﻿int start = 10;
+int stop = 99;
+
+int value = new Random().Next(start, stop+1);
+Console.WriteLine(value);
+
+int dig_first = value/10;
+int dig_second = value - dig_first * 10;
+
+if (dig_first < dig_second)
+{
+    Console.Write("max =" + dig_second);
+}
+else
+{
+    Console.Write("max =" + dig_first);
+}
