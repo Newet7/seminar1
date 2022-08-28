4333bf4fe9e1cba7761adb27c8eb266ffbf17ae6
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-28 20:29:59 +0300

    WorkGroup001

---
+++ WOrkGroup001_two_numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int reminder = 0;
+
+
+if(b % a == 0)
+{
+    Console.WriteLine ("b кратно a");
+}
+
+else
+{
+    reminder = a%b;
+    Console.WriteLine(reminder);
+}
