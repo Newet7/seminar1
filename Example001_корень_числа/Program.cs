f31061f5e7fecc242065476eff51349506aa244c
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-28 18:37:01 +0300

    Example001_fin?

--- Example001_корень_числа/Program.cs
+++ Example001_корень_числа/Program.cs
@@ -1,8 +1,21 @@
-﻿double a;
-string input;
+﻿//double a;
+//string input;
+//double result;
+//input = Console.ReadLine();
+//a = Double.Parse(input);
+//result = a * a;
+//Console.Write("result square a is", result);
+//Console.Write( a * a );
+
+double a;
 double result;
-input = Console.ReadLine();
-a = Double.Parse(input);
+string input = Console.ReadLine();
+a = double.Parse(input);
 result = a * a;
+Console.WriteLine(a);
 Console.Write("result square a is", result);
 Console.Write( a * a );
+
+//string a =Console.ReadLine();
+//double a = double.Parse(f);
+//Console.WriteLine(F);
