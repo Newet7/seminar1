bee5cd6fcbef3aee49bce6319db7548a90b27822
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-28 21:00:28 +0300

    work002_3number_2number_first+last

---
+++ WorkGroup002_3number-two_number/Program.cs
@@ -0,0 +1,10 @@
+﻿int start = 100;
+int stop = 999;
+
+int value = new Random().Next(start, stop+1);
+Console.WriteLine(value);
+
+int dig_first = value / 100;
+int dig_last = value%10;
+
+Console.WriteLine(dig_first + "" + dig_last);
