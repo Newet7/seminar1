dde65f683c5353f289d98c710e08acff9fb558e1
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-08-28 20:46:05 +0300

    work001_if_dont_go!!!

--- WOrkGroup001_two_numbers/Program.cs
+++ WOrkGroup001_two_numbers/Program.cs
@@ -1,15 +1,23 @@
-﻿int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
+﻿//int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+//int reminder = 0;
+int a;
+int b;
 int reminder = 0;
+string input1;
+string input2;
 
+input1 = Console.ReadLine();
+input2 = Console.ReadLine();
 
-if(b % a == 0)
+a = Int32.Parse(input1);
+b = Int32.Parse(input2);
+
+if (b % a == 0)
 {
-    Console.WriteLine ("b кратно a");
+    Console.Write("b кратно a");
 }
-
-else
-{
+else{
     reminder = a%b;
-    Console.WriteLine(reminder);
+    Console.WriteLine("не кратно, остаток" + reminder);
 }
