f71ac5a5ad47ccb4f08af07e4e3098e50f5a7282
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-09-04 11:45:12 +0300

    HW2 T1

---
+++ HW2/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int start = 100;
+int stop = 999;
+
+int value = new Random().Next(start, stop+1);
+Console.WriteLine(value);
+
+int dig_first_and_second = value / 10;
+int dig_last_first_and_second = dig_first_and_second%10;
+
+Console.WriteLine(dig_last_first_and_second);
